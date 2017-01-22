e8670f0af8862d25ccdcb40191600985a9c35812
Aleon Sully <aleonsullivan@outlook.com>
2017-01-22 19:14:39 +0000

    Done with the divide case

--- CmdCalc.java
+++ CmdCalc.java
@@ -3,6 +3,23 @@ public class CmdCalc{
     public static void main(String []args){
         
         System.out.println("Welcome to the Cmd calculator application");
+
+        System.out.println("type add for addition \n subtract for subtraction \n multiply for multiplication");
+
+        System.out.println("divide for division \n and exit to exit to app");
+
+        String operation = args[0];
+        
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        switch(operation.toLowerCase()){
+            
+            case "divide":
+                System.out.println("The quotient of the division is " + divide(x,y));
+            break;
+
+        }
     }
 
     public static int add(int x, int y){
