ddbce42aca547c343cb30f56d3ef7e2a47608d2c
Michael Delasi Saneke <michael@Michaels-MacBook-Air.local>
2017-01-22 19:44:10 +0000

    Lord's commit

--- CmdCalc.java
+++ CmdCalc.java
@@ -1,22 +1,26 @@
 public class CmdCalc{
     
-    public static void main(String []args){
+    public static void main(String args[]){
         
         System.out.println("Welcome to the Cmd calculator application");
 
         System.out.println("type add for addition \n subtract for subtraction \n multiply for multoplication");
         System.out.println("division for division \n and exit toexit the app");
 
-        String operation = args[0];
+        //String operation = args[0];
         int x = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
 
-        switch(operation.toLowerCase()){
-            
-            case "add":
+        if (args[0].equals("add")){
                 System.out.println("The sum of the two number is " + add(x,y));
-            break;
+        }
+            
+        else if (args[0].equals("subtract")) {
+            System.out.println("The difference of two numbers is " + subtract(x,y));
+        }
 
+        else if (args[0].equals("multiply")){
+            System.out.println("The product of two numbers is " + multiply(x,y));
         }
     }
 
@@ -24,6 +28,18 @@ public class CmdCalc{
         
         int sum = x + y;
         return sum;
+    }
+    
+    public static int subtract(int x, int y){
+      
+      int diff = x - y;
+      return diff;
+    }
+    
+    public static int multiply(int x, int y){
+      
+      int mult = x * y;
+      return mult;
     }
 
     
