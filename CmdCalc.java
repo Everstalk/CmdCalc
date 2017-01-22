3a0229db88b9e981c9953c488fe49c3fd27e9f87
Aleon Sully <aleonsullivan@outlook.com>
2017-01-22 15:59:11 +0000

    Just finished writing the code for the add method

---
+++ CmdCalc.java
@@ -0,0 +1,13 @@
+public class CmdCalc{
+    
+    public static void main(String []args){
+        
+        System.out.println("Welcome to the Cmd calculator application");
+    }
+
+    public static int add(int x, int y){
+        
+        int sum = x + y;
+        return sum;
+    }
+}
